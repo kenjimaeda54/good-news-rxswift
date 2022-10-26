53053c3b92d1967cdcf43a9e877c346a065b56a1
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-10-26 16:22:26 -0300

    Initial Commit

---
+++ Good News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Good News
+//
+//  Created by kenjimaeda on 26/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
