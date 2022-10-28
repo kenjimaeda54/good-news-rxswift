73169322653724f7553a56b6e9dadf622cec26c3
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-10-28 17:41:26 -0300

    feat: refactor with extensions request url

--- Good News/Models /Articles.swift
+++ Good News/Models /Articles.swift
@@ -15,3 +15,13 @@ struct Articles: Codable {
 	let title: String
 	let description: String?
 }
+
+extension ArticlesList {
+	
+	static var all: Resource<ArticlesList> = {
+		let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=e03da12b408445449464ceb16db4963a")!
+		return Resource(url: url)
+	}()
+	
+	
+}
